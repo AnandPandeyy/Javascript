57c52fd9cd460357a02b7f5caab8144cd70e72d0
Anand <anandpandeyofficial9939@gmail.com>
2026-07-25 17:01:04 +0530

    Initial commit

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+// console.log("Hello World");
+accountID = 1234
+accountemail = "andy@email.com"
+accountHolder = "Andy"
+
+console.table([accountID, accountemail, accountHolder])
---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert(3+3) // we are using nodejs , not browser
+
+// code readability should be high 
+
+// number => 2^53 
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+    let temp = null
+
+// undefined => value is not defined yet
+    let state; 
+
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
